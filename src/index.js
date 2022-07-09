7c8b34e3c9a395e80f0d775b88abde9f8088b15c
Shakhova14 <102754060+Shakhova14@users.noreply.github.com>
2022-07-10 01:57:02 +0300

    4th

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const countryInfo = document.querySelector(".country-info");
 
 
 let searchQuery = "";
+let dataAll = null;
 
 inputEl.addEventListener("input", debounce(startSearchCountries, DEBOUNCE_DELAY));
 
@@ -23,14 +24,13 @@ return searchQuery;
 }
 
 function clearContent(){
-  inputEl.value = '';
   countryList.innerHTML = '';
   countryInfo.innerHTML = '';  
 }
 function fetchCountries() {
  fetch(`https://restcountries.com/v3.1/name/${searchQuery}?fields=name,capital,population,flags,languages`)
 .then(response => {return response.json()}).then(data => {
-
+dataAll = data;
 
     if (data.length > 10) {
         clearContent();
@@ -40,29 +40,50 @@ function fetchCountries() {
     else if (data.length > 1 && data.length <= 10) {
         clearContent();
         console.log(data)
-     Notify.success('Норм, нашел пачку!');
-    const markup = data.map((el) => `<div class = "info"><img src="${el.flags.svg}"/><p>${el.name.official}</p></div>`)
-.join("");
-        countryInfo.insertAdjacentHTML("beforeend", markup);
+    Notify.success('Норм, нашел пачку!');
+    pushLotCounries()
+       
     }  else if (data.length === 1) { 
-        clearContent();
-        const markupLot = data.map((el) => ` <div class = "list"><img src="${el.flags.svg}" width = 10% /><p>${el.name.official}</p>
-        <ul>
-          <li>Capital:"${el.capital}"</li>
-          <li>Population:"${el.population}"</li>
-          <li>Languages:"${el.languages}"</li>
-        </ul></div>`)
-        .join("");
-        countryList.insertAdjacentHTML("beforeend", markupLot);
-       Notify.success('Нашел Адын!'); }
-    else {
+      clearContent();
+      pushOneCountry (data[0])
+      Notify.success('Нашел Адын!'); }
+      else {
         clearContent();
       Notify.failure('Все пропало, давай по новой!');
     }
   })
 }
 
+function pushLotCounries() {
+   const markupLot = dataAll.map((el) => `<div class = "info"><img src="${el.flags.svg}"/><p>${el.name.official}</p></div>`)
+        .join("");
+      countryList.insertAdjacentHTML("beforeend", markupLot);
+}
 
+
+function pushOneCountry(country) {
+  console.log(country);
+  console.log(dataAll);
+  countryInfo.innerHTML = ` <div class = "list"><img src="${country.flags.svg}" width = 10% /><p>${country.name.official}</p>
+          <ul>
+            <li>Capital:"${country.capital}"</li>
+            <li>Population:"${country.population}"</li>
+            <li>Languages:"${Object.values(country.languages).join(', ')}"</li>
+          </ul></div>`;
+}
+// function pushOneCountry () {
+  
+//   const markup = dataAll.map((el) => ` <div class = "list"><img src="${el.flags.svg}" width = 10% /><p>${el.name.official}</p>
+//         <ul>
+//           <li>Capital:"${el.capital}"</li>
+//           <li>Population:"${el.population}"</li>
+//           <li>Languages:"${el.languages.join(', ')}"</li>
+//         </ul></div>`)
+//         .join("");
+//         console.log(dataAll)
+//         // console.log(Object.values(el.languages));
+//          countryInfo.insertAdjacentHTML("beforeend", markup);
+// }
 // const markup = galleryItems
 // .map((el) => `<a class="gallery__item" href="${el.original}">
 // <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
